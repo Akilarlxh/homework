ad488186594f973e9260a90b60049a5a9e7b21b7
bibibi <wztxy89@163.com>
2016-11-25 22:36:36 +0800

    123

---
+++ kb5-25 折半查找法.c
@@ -0,0 +1,36 @@
+﻿/*kb5-25*/ 
+/*折半查找法 (只能对有序数列进行查找),由键盘输入
+一个数，在10个从小到大排好序的数列中查找该数，若
+存在则指出下标，不存在则提示没有找到*/ 
+/* mid中间 top上界 bot下界*/
+#include <stdio.h>
+#define N 10
+int main()
+{
+	int num[N]={1,4,7,13,16,19,28,36,49,60};
+	int n,mid,bot,top,flag=0; 
+	scanf("%d",&n);
+	mid=N/2;
+	bot=0;
+	top=N-1;
+	while(flag==0 && bot<=top) { 
+		mid=(bot+top)/2;
+		if(n==num[mid]){
+			flag=1;
+			break;
+		}
+		else if(n<num[mid]){
+			top=mid-1;
+		}
+		else{
+			bot=mid+1;
+		}
+	}
+	if(flag){
+		printf("找到了，这个数的下标是%d",mid);
+	}
+	else{
+		printf("没有找到"); 
+	}
+	
+}   
---
+++ kb5-26 插入法.c
@@ -0,0 +1,28 @@
+﻿/*kb5-26*/
+/*插入法把一个给定数据x按大小插入到已经
+排好序的数组中，插入后该数组任然有序*/
+#include <stdio.h>
+#define N 10
+int main()
+{
+	int num[N+1]={1,3,5,7,9,12,15,19,22,33};
+	int x,record-1,cnt;
+	scanf("%d",&x);
+	for(cnt=0;cnt<N-1;cnt++){
+		if(x>num[cnt] && x<num[cnt+1]){
+			record=cnt;
+		}
+	}//判断是否满足，并记录cnt下标 
+	if(record==-1) {
+		num[N]=x;
+	}
+	else{
+		for(cnt=N;cnt>record+1;cnt--){
+			num[cnt]=num[cnt-1];
+		}
+		num[record+1]=x;
+	}
+	for(cnt=0;cnt<N+1;cnt++){
+		printf("%d ",num[cnt]);
+	}
+}
