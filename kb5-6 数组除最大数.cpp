3c747f764f2e0f00d7466efd5a52ebc0273b0d53
bibibi <wztxy89@163.com>
2016-11-25 20:30:50 +0800

    kb5-6 数组除最大数
    输入一个5行6列的数组，将每一行的所有元素都除以该行上绝对值最大的元素，然后输出数组

---
+++ kb5-6 数组除最大数.cpp
@@ -0,0 +1,29 @@
+﻿/*kb5-6*/
+/*输入一个5行6列的数组，将每一行的所有元素都除
+以该行上绝对值最大的元素，然后输出数组*/
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float num[5][6],max[5]={0};
+	int m,n;
+	for(m=0;m<5;m++){
+		for(n=0;n<6;n++){
+			scanf("%f",&num[m][n]);
+		}
+	}
+	for(m=0;m<5;m++){
+		for(n=0;n<6;n++){
+			max[m]=max[m]>fabs(num[m][n])?
+			max[m]:num[m][n];
+		}
+	}	
+	for(m=0;m<5;m++){
+		for(n=0;n<6;n++){
+			num[m][n]=num[m][n]/max[m];
+			printf("%f ",num[m][n]);
+		}
+		printf("\n");
+	}
+	return 0;
+ } 
---
+++ sy4-7 阶乘.c
@@ -0,0 +1,27 @@
+﻿/*sy4-7*/
+/*编程计算1-1/3!+1/5!-1/7!+...+(-1)^(n-1)/(2n-1)!，
+其中n的值在程序运行时由键盘输入*/
+#include <stdio.h>
+int fact(float number);
+int main()
+{
+	float cnt,num=0;
+	float n;
+	scanf("%f",&n);
+	for(cnt=1;cnt<=n;cnt++){
+		num+=pow(-1,cnt-1)/fact(2*cnt-1);
+	}
+	printf("%f\n",num);
+}
+
+int fact(float number)
+{
+	float num=1;
+	float i;
+	for(i=1;i<=number;i++){	
+		num*=i;	
+	}
+	return num;
+}
+
+
