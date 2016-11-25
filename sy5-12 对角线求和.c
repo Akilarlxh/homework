57c6b147b19ec5a36f2d484481d52ed355a188d0
bibibi <wztxy89@163.com>
2016-11-25 21:10:11 +0800

    1

---
+++ sy5-12 对角线求和.c
@@ -0,0 +1,32 @@
+﻿/*sy5-12*/
+/*编程输入一个n*n矩阵中各元素的值，从键盘输入n
+的值,并求出两条对角线上各元素的和*/
+/*该题需要考虑n是奇数还是偶数，奇数的话中间数
+会被重复记两次*/
+
+#include <stdio.h>
+int main()
+{
+	int N,m,n,sum1=0,sum2=0,sum;
+	printf("请输入N的值：\n");
+	scanf("%d",&N);
+	int num[N][N];//在c99里可以这么做
+	for(m=0;m<N;m++){                                
+		for(n=0;n<N;n++){                              
+			scanf("%d",&num[m][n]);                    
+		}                                   
+	}//二维数组写入数据
+	for(m=0,n=0;m<N;m++,n++){
+		sum1=sum1+num[m][n];
+	}
+	for(m=0,n=N-1;m<N;m++,n--) {
+		sum2=sum2+num[m][n];
+	}
+	if(N%2==0){
+		sum=sum1+sum2;
+	}
+	else{
+		sum=sum1+sum2-num[N/2][N/2];
+	}
+	printf("%d",sum);
+} 
---
+++ sy5-15 随机数排序.c
@@ -0,0 +1,49 @@
+﻿/*sy5-15*/
+/*编写程序，随机产生20个两位正整数，将其中的前
+10个数字从小到大排序，后10个数字从大到小排序，
+然后按每行10个输出这20个数字 */ 
+
+#include <stdio.h> 
+#include <stdlib.h>
+int main()
+{
+	int num[20]={0},m,n;
+	for(m=0;m<20;m++){
+		while(num[m]<10)
+			num[m]=rand()%100;
+			//0-99 题目要求10-99
+	} 
+	for(m=0;m<20;m++){
+		printf("%d ",num[m]);
+		if(m==9) puts("\n");
+	} 
+	puts("\n");
+	/*给前10个值从小到大排序;
+	给后10个值从大到小排序;
+	输出这20个值；*/ 
+	for(m=0;m<10;m++) {
+		for(n=m;n<10;n++){
+			if(num[m]>num[n]){
+				int t;
+				t=num[m];
+				num[m]=num[n];
+				num[n]=t;
+			}
+		}
+	}
+	for(m=10;m<20;m++) {
+		for(n=m;n<20;n++){
+			if(num[m]<num[n]){
+				int t;
+				t=num[m];
+				num[m]=num[n];
+				num[n]=t;
+			}
+		}
+	}
+	for(m=0;m<20;m++) {
+	printf("%d ",num[m]);
+	if(m==9) puts("\n");
+	}
+	
+}
