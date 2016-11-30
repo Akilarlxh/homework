ce0cd31254e07d0f204bfa4aa052f25957f23ab4
bibibi <wztxy89@163.com>
2016-11-30 23:21:16 +0800

    sy6-12 判断回文
    sy6-12 判断回文

---
+++ sy6-12 判断回文.c
@@ -0,0 +1,21 @@
+﻿/*sy6-12*/
+/*编写一个程序，判断输入的一个字符串是否回文，
+若是回文则输出YES，否则输出NO。所谓回文是顺读
+和倒读都一样的字符串*/
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	int cnt,flag=1;
+	char str[100];
+	scanf("%s",str);
+	for(cnt=0;cnt<=strlen(str)/2;cnt++){
+		if(str[cnt]!=str[strlen(str)-1-cnt]){
+			flag=0;
+		}
+	}
+	if(flag==0)
+		printf("NO");
+	else
+		printf("YES");
+}
