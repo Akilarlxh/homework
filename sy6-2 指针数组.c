b64cd5d6cfc0bb2ef4db46a2abdce041b3e2210e
bibibi <wztxy89@163.com>
2016-11-30 23:21:23 +0800

    sy6-2 指针数组
    sy6-2 指针数组

---
+++ sy6-2 指针数组.c
@@ -0,0 +1,20 @@
+﻿/*sy6-2*/
+/*输出所有数并求和，用指针*/
+#include <stdio.h>
+#define N 3
+#define M 4
+int main()
+{
+	int a[N][M]={1,3,5,7,9,11,13,15,17,19,21,23};
+	int *p[N],i,j,k=0,sum=0;
+	for(int i=0;i<N;i++){
+		p[i]=a[i];
+	}
+	for(i=0;i<N;i++){
+		for(j=0;j<M;j++){
+			sum+=*p[i];
+			printf("%d ",*p[i]++);
+		}
+	}
+	printf("%d",sum);
+ } 
